f72d1b99bbfbef1a48bcc06741f5cd062959d71d
Diego Guarnieri <diego.guarnieri@gmail.com>
2018-12-09 18:47:42 -0800

    .

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Vancouver',
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
 
     'cipher' => 'AES-256-CBC',
 
